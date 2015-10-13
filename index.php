ab037d75aaaf18234b793cda3e7ac2bf3942f1ca
Nicolás Fernández <nicof05@gmail.com>
2015-10-13 16:20:49 -0300

    Cambio del origen de los archivos de bootstrap (css y js) (2)

--- index.php
+++ index.php
@@ -500,7 +500,7 @@
 			<!-- Include all compiled plugins (below), or include individual files as needed -->
 			<!-- Latest compiled and minified JavaScript 
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
-                        <script src="http://udc.edu.ar/inscripciones/bootsatrap/js/bootstrap.min.js"></script>
+                        <script src="http://udc.edu.ar/inscripciones/bootstrap/js/bootstrap.min.js"></script>
 		</div>
 		<hr>
 	  </div>
